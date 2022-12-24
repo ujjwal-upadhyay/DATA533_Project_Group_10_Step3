acd4102f2e34da25717d81d6900ee7e99f6678a1
Ujjwal Upadhyay <33262519+ujjwal-upadhyay@users.noreply.github.com>
2022-12-24 00:27:26 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+name='fruit_purchase_UX',
+version='0.1',
+packages=find_packages(exclude=['Test*']),
+license='MIT',
+description='A test python package’,
+url='URL to GitHub repository',
+author='Ujjwal Upadhyay and Xiaoxiao Fu',
+author_email='upadhyay.ujjwal007@gmail.com'
+)
